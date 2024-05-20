5cb6eb383f30327fa6e0e5953fde98631691e0be
marianobaroni23456 <marianobaroni23456@gmail.com>
2024-05-20 15:47:01 -0300

    cambios locales en gitingore

--- Inscripciones/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Inscripciones.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
